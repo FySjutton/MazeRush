cb54c632b65262f73eccadaec24fa5be5abf6f38
Fy17 <rubik.sten@gmail.com>
2026-05-22 22:31:39 +0200

    Initial Commit

---
+++ src/main/java/avox/se/mazeRush/MazeRush.java
@@ -0,0 +1,16 @@
+package avox.se.mazeRush;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MazeRush extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
